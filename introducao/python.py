d0f140eaaed63d170c678c6df243231585bca67e
ArthurOReis <arthuroreis2005@gmail.com>
2023-01-06 19:31:31 -0300

    Adicionando arquivo python

---
+++ introducao/python.py
@@ -0,0 +1,24 @@
+# O Python possui uma série de comandos e funções, onde será demonstrado
+
+print()
+
+# 'print()' -> Printa mensagens que podem ser escritos dentro de aspas, nos parênteses, tanto em forma de string como boolean, int, double, float...
+# Também pode printar valores de variáveis.
+
+variavelA = "Variável"
+variavelB = 123
+variavelC = True
+variavelD = 0.5
+
+# Variáveis são elementos que guardam valores dentro deles, onde os mesmos podem ser de qualquer tipo
+# Também podem guardar sequências, onde no mundo Python há três tipos de sequências
+# Porém o que diferencia Python das outras linguagens é o fato de poder guardar valores diferentes em uma única sequência
+
+variavelF = [1, 2, 3, 4] #Lista/List
+variavelE = {1, 2, 3, 4} #Dicionário/Dictionary
+variavelG = (1, 2, 3, 4) #Tupla/Tuple
+
+# Os três tipos de sequências apesar de iniciarem no index '0' como muitas linguagens de programação, trazem características distintas:
+# Lista: Sequência acompanhada de index com valores que podem ser mudados
+# Dicionário: Sequência de chaves que representam a index e armazenam um valor nelas
+# Tupla: Sequência acompanhada de index com valores únicos que não podem ser alterados
