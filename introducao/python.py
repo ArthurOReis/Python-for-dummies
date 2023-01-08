8b079874ecc004216a442d083e4e3b67ae902418
ArthurOReis <arthuroreis2005@gmail.com>
2023-01-08 16:57:49 -0300

    Introduzindo sobre looping

--- introducao/python.py
+++ introducao/python.py
@@ -45,16 +45,35 @@ else: # Senão
 
 # Elif: É outra condição antes do 'else' que faz outra verificação caso o primeiro 'if' seja inválido:
 
-if 1 == 1.1:
-    print("1 é igual a 1.1!")
-elif 1 == 1.0:
-    print("1 é igual a 1.0!")
-else:
-    print("1 é igual a 1!")
+if 1 == 1.1: # Se 1 for igual a 1.1
+    print("1 é igual a 1.1!") # Printe essa mensagem
+    
+elif 1 == 1.0: # Porém se 1 for igual a 1.0
+    print("1 é igual a 1.0!") # Printe essa mensagem
+    
+else: # Caso nenhuma nenhuma das condições acima forem verdadeiras
+    print("1 é igual a 1!") # Printe essa mensagem
 
-# Outro tipo de condição são loopings que terminam se uma condição for verdadeira,
+# Outro tipo de condição são loopings que terminam se uma condição for verdadeira, sendo elas 'for' e 'while'
+# For: Estrutura de repetição que itera sobre elementos de uma sequência (Como uma string, uma lista, tupla ou dicionário):
 
-#i = 0    
-#while i < 100:
-#    i += 1
-#    print(i) 
+banana = "Banana" # Variável 'banana' com valor string "Banana"
+
+for letra in banana: # Para cada letra em banana
+    print(letra) # Printe as letras
+    
+tuplas = ('tupla1', 'tupla2', 'tupla3') # Variável 'tuplas' com valor de uma sequência de tupla
+
+for tupla in tuplas: # Para cada tupla em 'tuplas'
+    print(tupla) # Printe todas 'tupla' em tuplas
+    
+informacoes_pessoais = {'Nome': 'Arthur', 'Idade': 16, 'Cidade': 'São Paulo'} # Variável 'informacoes_pessoais' com chaves e seus valores
+
+for chave, valor in informacoes_pessoais.items(): # Para cada chave e valor listado em 'informacoes_pessoais'
+    print("{}:{}".format(chave, valor)) # Printe todas as chaves e valores
+    
+# Porém o 'for' não precisa necessariamente antes criar uma variável para ser inicializada
+# É possível fazer uma contagem progressiva usando o método 'range()', que se inicia por padrão no index '0'
+
+for i in range(10): # Para cada variável 'i' em uma índice de 10 números
+    print(i) # Printe 10 vezes 'i', aumentando seu valor a cada 'print(i)'
