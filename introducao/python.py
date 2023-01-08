cf8e87bade7e9e7b5ef96c7e1973850622e50c06
ArthurOReis <arthuroreis2005@gmail.com>
2023-01-08 18:49:45 -0300

    Adicionando ' na linha 85

--- introducao/python.py
+++ introducao/python.py
@@ -82,5 +82,5 @@ for i in range(10): # Para cada variável 'i' em uma índice de 10 números
 # E assim como "for i in range(10)", é possível criar um loop que recebe uma variável com valor incial '0'
 
 while i < 100: # Enquanto 'i' for menor que 100
-    print(i) # Printe 'i
+    print(i) # Printe 'i'
     i += 1 # E adicione 1 no valor de 'i'
